805112d748b4781ced170042da0866b94670e28c
Steffen Tröster <steffen.troester@googlemail.com>
2014-10-10 15:10:04 +0200

    uncomment tricky modules refs #1

--- pixmodules/__init__.py
+++ pixmodules/__init__.py
@@ -2,5 +2,5 @@ from pixmodule import PixModule
 from gameoflife import GameOfLife
 from snake import Snake
 from pong import Pong
-from textview import TextView
-from smilie import Smilie
+#from textview import TextView
+#from smilie import Smilie
